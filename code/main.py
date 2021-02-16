3f285e663f95dfad81bfd13623f890daa174a0c1
Pau Serra <pau.serra.bergeron@gmail.com>
2021-02-16 10:38:25 +0100

    Added log file to follow the evolution of the process

--- code/main.py
+++ code/main.py
@@ -12,7 +12,7 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
-from datetime import datetime
+from datetime import datetime, date
 
 #nltk.download('stopwords')
 #nltk.download('wordnet')
@@ -70,35 +70,60 @@ def main():
     3) clean lyrics
     4) add the list of the unique words in each song
     """
+    file_name = "lyrics_extraction_" + str(date.today()) + ".log"
+    f = open("../../" + file_name, "a")
+    f.write("################################################################\n")
+
     songs_df = pd.read_csv("../data/songs_all.csv")
     words_lyrics = []
 
     start = datetime.now()
+    f.write("START: " + str(start) + "\n")
+    f.close()
+
+    words_lyrics = ["Pending" for i in range(len(songs_df))]
 
     for i in range(len(songs_df)):
+        f = open("../../" + file_name, "a")
         print("Song: " + str(i+1) + "/" + str(len(songs_df)))
+        f.write("Song: " + str(i+1) + "/" + str(len(songs_df)) + "\n")
         song_name = songs_df.name[i]
         artist_name = songs_df.artist_1[i]
         if "-" in song_name:
             song_name = song_name.split(" - ")[0]
         song = genius.search_song(song_name, artist_name)
         if song == None:
-            words_lyrics.append(None)
+            words_lyrics[i] = None
         else:
             lyrics_song = song.lyrics
             if len(lyrics_song) == 0:
-                words_lyrics.append(None)
+                words_lyrics[i] = None
             else:
                 cleaned_lyrics = clean_lyrics(lyrics_song)
-                words_lyrics.append(list(set(lyrics_to_words(cleaned_lyrics).split())))
+                words_lyrics[i] = list(set(lyrics_to_words(cleaned_lyrics).split()))
+        if (i+1)%100 == 0:
+            songs_df['words_lyrics'] = words_lyrics
+            songs_df.to_csv("../data/songs_lyrics.csv", index=False)
+            inter = datetime.now()
+            print("Time spend:", inter-start, "- Average by song:", (inter-start)/(i+1))
+            f.write("Time spend: " + str(inter-start) + " - Average by song: " + str((inter-start)/(i+1)) + "\n")
+        f.close()
 
     stop = datetime.now()
 
     print("Time spend:", stop-start, "- Average by song:", (stop-start)/len(songs_df))
 
+    f = open("../../" + file_name, "a")
+
+    f.write("Time spend: " + str(stop-start) + " - Average by song: " + str((stop-start)/(i+1)) + "\n")
+    f.write("Finished!!!" + "\n")
+    f.write("################################################################" + "\n")
+
     songs_df['words_lyrics'] = words_lyrics
 
     songs_df.to_csv("../data/songs_lyrics.csv", index=False)
+
+    f.close()
 
 if __name__ == "__main__":
     main()
